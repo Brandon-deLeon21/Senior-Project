53eb268badcfe7ecdca9cee5d89716fd377653d3
Brandon-deLeon21 <brandon.deleon721@gmail.com>
2024-06-08 14:36:23 -0600

    Add files via upload

--- sp 3d final.py
+++ sp 3d final.py
@@ -1,5 +1,4 @@
 
-
 from mpl_toolkits import mplot3d
 
 from numpy import sin,cos,pi,array,sqrt,arctan,arccos,arcsin,arctan2,zeros
@@ -46,10 +45,7 @@ class Planet:
         
     def setupV(self,dt,others):
         derv = self.deriv(self.vars,others)
-        #print(self.vars)
-        #print(derv)
         self.vars[2:] += .5*dt*derv[2:]
-        #print(self.vars)
     
     def getApa(self):
         r = sqrt(array(self.x)**2 + array(self.y)**2)
@@ -67,12 +63,12 @@ class Planet:
             
     def getPeri(self):
         r = sqrt(array(self.x)**2 + array(self.y)**2)
-        index = []#0]
+        index = []
         for i in range(len(r)-1):
             if(r[i-1]>r[i] and r[i]<r[i+1]):
                 index.append(i)
-        peri = []#r[0]]
-        periCom = []#array([self.x[0],self.y[0]])]
+        peri = []
+        periCom = []
         for i in index:
             peri.append(r[i])
             periCom.append(array([self.x[i],self.y[i]]))
@@ -87,16 +83,12 @@ class Planet:
         return index
         
     def deriv(self,vars,others):
-        #isida = False
         x = vars[0]
         y = vars[1]
         z = vars[2]
         vx = vars[3]
         vy = vars[4]
         vz = vars[5]
-        Relvx = vx
-        Relvy = vy
-        Relvz = vz
         G = 1.98524*10**(-29) #AU/yr
         
         dx = vx
@@ -109,8 +101,7 @@ class Planet:
 
         for body in others:
             r2 = (x-body.x[-1])**2 + (y-body.y[-1])**2 + (z-body.z[-1])**2
-            a = -G*body.m/r2  #need to change to G*M
-            #print("acceleration",a)
+            a = -G*body.m/r2  
             
             dvx += a*(x-body.x[-1])/sqrt(r2)
 
@@ -118,7 +109,6 @@ class Planet:
             
             dvz += a*(z-body.z[-1])/sqrt(r2)
 
-        #print("dvx",atx,"dvy",aty)
         return array([dx,dy,dz,dvx,dvy,dvz])
     
         
@@ -131,8 +121,6 @@ class Planet:
             #print(self.vars[2:])
         self.vars[:3] += dt*self.vars[3:]
         k = dt*self.deriv(self.vars,others)
-        #print(self.vars)
-        #print(k)
         #self.pE.append(- 1.98524*10**(-29)*1.9885*10**30*5.9722*10**24/
                        #sqrt((self.vars[0] - others[0].vars[0])**2 + (self.vars[1] - others[0].vars[1])**2))
         self.vars[3:] += .5*k[3:]
@@ -140,8 +128,6 @@ class Planet:
         #self.kE.append(.5*self.m*(self.vars[2]**2 + self.vars[3]**2))
         
         self.vars[3:] += .5*k[3:]
-        #print(self.vars)
-        #print("k:",k)
         """use new pos to find new velocity
         k2 = self.deriv(k,others)
         self.vars[2:] += dt*k2[2:]"""
@@ -153,53 +139,43 @@ class Planet:
         self.vx.append(self.vars[3])
         self.vy.append(self.vars[4])
         self.vz.append(self.vars[5])
-    
-    """def graph(self,s=0,f=-1):
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.plot3D(self.x[s:f],self.y[s:f],self.z[s:f],',')
-        #plt.show()"""
+
     
     def graphEnergy(self):
         plt.plot(self.t[1:],self.energy[1:])
         plt.show()
-        print("here")
         
 class Probe(Planet):
     def setupStages(self):
         self.idaApproach = False
-        self.phi0 = 0
+        self.phi0 = 0 
         self.th0 = 0
-        self.stage1 = 1
+        self.stage1 = 1 #stage1 thrust time counter for thrust acceleration
         self.stage2 = 0
-        self.ts1 = 47005
-        self.s1dur = 80
-        self.ts2 = self.ts1 + self.s1dur+1
-        self.s2dur = 195
-        self.ts2f = self.ts2+self.s2dur+1
-        self.taa = 189716
-        self.taadur = 25#30
-        self.taa2 = 194689
-        self.taadur2 = 335#30
-        self.tdropao = 200850
-        self.tal = 201480
-        self.taldur = 16
-        self.tadep = 408250
-        self.tadepdur = 667
-        self.tea = 509888
-        self.teadur = 338
-        self.tap = 520003
+        self.ts1 = 47005 #stage1 start time
+        self.s1dur = 80 #stage1 thrust duration
+        self.ts2 = self.ts1 + self.s1dur+1 #stage2 start time
+        self.s2dur = 195 #stage2 thrust duration
+        self.ts2f = self.ts2+self.s2dur+1 #changed dt for more controled burntime/flight path
+        self.taa = 189716 #time to start thrusters for astroid approach
+        self.taadur = 25 #time astroid approach duration
+        self.taa2 = 194689 #second stage of astroid approach (changing thrust angles)
+        self.taadur2 = 335 #second stage duration
+        self.tdropao = 200850 #time to drop out of asteroid orbit
+        self.tal = 201480 #time to start launding thrusters
+        self.taldur = 16 #landing duration
+        self.tadep = 408250 #time asteroid launch
+        self.tadepdur = 667 #thrust depature duration
+        self.tea = 509888 #time start thrusters to enter earth orbit
+        self.teadur = 338 
+        self.tap = 520003 #time for second stage of thruster to enter earth orbit
         self.tapdur = 495
         
     def setupV(self,dt,others,at=0):
         derv = self.deriv(self.vars,others,at)
-        #print(self.vars)
-        #print(derv)
         self.vars[2:] += .5*dt*derv[2:]
-        #print(self.vars)
         
     def deriv(self,vars,others,at,yaw=0,pitch=0):
-        #isida = False
         yaw *= pi/180
         pitch *= pi/180
         dphi = 0
@@ -227,7 +203,7 @@ class Probe(Planet):
 
         for body in others:
             r2 = (x-body.x[-1])**2 + (y-body.y[-1])**2 + (z-body.z[-1])**2
-            a = -G*body.m/r2  #need to change to G*M
+            a = -G*body.m/r2  
             #print("acceleration",a)
             if body.name == "Earth" and at!=0 and self.inOrbit:# and a>.01:
                 Relvx = vx - body.vars[3] - 0.0608575
@@ -241,11 +217,6 @@ class Probe(Planet):
                 thpos = arctan(sqrt((x - body.x[-1])**2 + (y - body.y[-1])**2)/(z - body.z[-1]))
                 phipos = arctan2(x-body.x[-1],y - body.y[-1])*(-1)+pi/2
                 tempv = sqrt(Relvx**2 + Relvy**2 + Relvz**2)
-                """print((x-body.x[-1]), + (y-body.y[-1]), (z-body.z[-1]))
-                print("a-p V:",sqrt(Relvx**2 + Relvy**2 + Relvz**2))
-                print("a-p th:",arctan(sqrt((Relvx)**2 + (Relvy)**2)/(Relvz))*180/pi , thpos*180/pi)
-                print("a-p phi:",(arctan2(Relvx,Relvy)*(-1)+pi/2)*180/pi -180 - phipos*180/pi)
-                """
             
             dvx += a*(x-body.x[-1])/sqrt(r2)
 
@@ -261,25 +232,15 @@ class Probe(Planet):
         if self.idaApproach and at!=0:    #pointing self in direction of velocity, need to change idaApp name to velDir or something
                 self.th0 = arctan2(sqrt((Relvx)**2 + (Relvy)**2),(Relvz))
                 self.phi0 = arctan2(Relvx,Relvy)*(-1)+pi/2
-                print("asdfargaerzfgsrthwsdfvs",i)
-                #print("ang",self.phi0*180/pi,self.th0*180/pi)
-                #print(vx,vy,vz)
-                #print(vx/vy)
                 
-        if self.earthAp:
-            print(at)
-            print(self.phi0*180/pi,self.th0*180/pi)
-        
         if at!=0:
             atx = at*cos(self.phi0 + yaw)*sin(self.th0 + pitch)
             aty = at*sin(self.phi0 + yaw)*sin(self.th0 + pitch)
             atz = at*cos(self.th0 + pitch)
-            print(atx,aty,atz)
             
             if self.idaApproach:
                 th = arctan2(sqrt((atx)**2 + (aty)**2),(atz))
                 phi = arctan2(atx,aty)*(-1)+pi/2
-                #print("at",atx,aty,atz)#("ang at",phi*180/pi,th*180/pi)
             
             alpha = 0
             if yaw!=0 or pitch!=0:
@@ -336,8 +297,6 @@ class Probe(Planet):
             #print(self.vars[2:])
         self.vars[:3] += dt*self.vars[3:]
         k = dt*self.deriv(self.vars,others,at,yaw,pitch)
-        #print(self.vars)
-        #print(k)
         #self.pE.append(- 1.98524*10**(-29)*1.9885*10**30*5.9722*10**24/
                        #sqrt((self.vars[0] - others[0].vars[0])**2 + (self.vars[1] - others[0].vars[1])**2))
         self.vars[3:] += .5*k[3:]
@@ -383,43 +342,28 @@ class Probe(Planet):
                 self.vars[4] = v*sin(self.phi0)*sin(self.th0) + earth.vars[4] + 0.0558099#+ 3.09705*cos(th0)*10**-9
                 self.vars[5] = v*cos(self.th0) + earth.vars[5] - 0.0242669
                 print("p init dir:",v*cos(self.phi0),v*sin(self.phi0))
-                #print(self.vars[3] - earth.vars[3],self.vars[4] - earth.vars[4])
                 probe.setupV(dt,array([earth,sun,jupiter]),76965.3)
-                #print(self.vars[3] - earth.vars[3],self.vars[4] - earth.vars[4])
+
             #start thrusters after 536
-            elif self.ts1 < i <(self.ts2):#1670 < i <2400:#was6243-6252
+            elif self.ts1 < i <(self.ts2):
                 probe.leapFrog(dt,array([earth,sun,jupiter]),(76965.3+(540410.9-76965.3)/self.s1dur*self.stage1),yaw=0,pitch=1.0)#thrust acceleration equation here already taking into account the change in mass
                 self.stage1 += 1
-                print(self.stage1-1)
-                #print(i, dt, sqrt(self.vars[3]**2 + self.vars[4]**2 + self.vars[5]**2))
             elif (self.ts2) < i <(self.ts2f):
-                #probe.inOrbit = False
                 probe.leapFrog(dt,array([earth,sun,jupiter]),(25398.2+((121694.4-25398.2)/260)*self.stage2),yaw=7.75,pitch=12.25)#1.35)
                 self.stage2 += 1
-                #print(stage2)
-                #print(i, dt)
             elif i==(self.ts2f):
                 probe.leapFrog(dt,array([earth,sun,jupiter]),(25398.2+((121694.4-25398.2)/260)*self.stage2),yaw=7.75,pitch=12.1)#51099.08/2,-15)#47620
-                #print(stage2)
-                #print(i, dt)
                 dt = .00001
                 probe.inOrbit = False
-                #print((TIME-TimeCount))
                 print("launch time:", (TIME - TimeCount)*365.2425*24*3600)
             elif (self.taa) < i <=(self.taa + self.taadur):
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),18503.7,yaw=-1.5,pitch=2.2)
             elif (self.taa2) < i <(self.taa2 + 93):
-                #probe.inOrbit = False
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),18503.7,yaw=30,pitch=90-self.th0*180/pi)
-                #print(i,dt)
             elif (self.taa2+93) < i <(self.taa2 + self.taadur2):
-                #probe.inOrbit = False
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),18503.7,yaw=-14.7,pitch=90-self.th0*180/pi)
-                #print(i,dt)
             elif i== (self.taa2 + self.taadur2):
-                #probe.inOrbit = False
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),18503.7,yaw=-14.5,pitch=90-self.th0*180/pi)
-                #print(i,dt)
             elif (self.tdropao) < i <(probe.tdropao+2):
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),-18503.7)
             elif (self.tal) < i <(probe.tal + probe.taldur):
@@ -429,7 +373,7 @@ class Probe(Planet):
             else:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]))
         if probe.tal + probe.taldur<=i<self.tadep:
-            self.x.append(ida.vars[0])#+0.00004258750455597226)
+            self.x.append(ida.vars[0])
             self.y.append(ida.vars[1])
             self.z.append(ida.vars[2])
             self.vx.append(ida.vars[2])
@@ -451,21 +395,15 @@ class Probe(Planet):
                 self.vars[4] = v*sin(self.phi0)*sin(self.th0) + ida.vars[4] #+ 0.0558099#+ 3.09705*cos(th0)*10**-9
                 self.vars[5] = v*cos(self.th0) + ida.vars[5] #- 0.0242669
                 print("p init dir:",v*cos(self.phi0),v*sin(self.phi0))
-                #print(self.vars[3] - earth.vars[3],self.vars[4] - earth.vars[4])
                 probe.setupV(dt,array([earth,sun,ida,jupiter]),76965.3)
             elif self.tadep < i < self.tadep+self.tadepdur:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),18503.7,yaw=0,pitch=0)
-                print(self.phi0*180/pi,self.th0*180/pi)
             elif self.tea < i < self.tea+301:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),-18503.7,yaw=-3,pitch=-10)
-                print(self.phi0*180/pi,self.th0*180/pi)
             elif self.tea+300 < i < self.tea+self.teadur:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),-18503.7,yaw=-1,pitch=-10)
-                print(self.phi0*180/pi,self.th0*180/pi)
             elif self.tap < i < self.tap+self.tapdur:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]),-18503.7,yaw=15,pitch=0)
-                print(i)
-                #print(self.phi0*180/pi,self.th0*180/pi)
             else:
                 probe.leapFrog(dt,array([earth,sun,jupiter,ida]))
         
@@ -482,7 +420,6 @@ def graph(title,bodies,s=0,f=-1,tilt=30,rot=-100):
     ax = plt.axes(projection='3d')
     i = 0
     for body in bodies:
-        #print(body.name)
         ax.plot3D(body.x[s:f],body.y[s:f],body.z[s:f],colors[i])
         i += 1
     ax.set_xlabel('$X(au)$')
@@ -492,7 +429,6 @@ def graph(title,bodies,s=0,f=-1,tilt=30,rot=-100):
     ax.axes.set_zlim3d(bottom=-.6, top=.6) 
     ax.view_init(tilt, rot)
     plt.show()
-    #print("here")
     
 def CenteredGraph(title,center,orbital,R,s=0,f=-1,tilt=30,rot=-100):
     fig = plt.figure()
@@ -516,8 +452,6 @@ def CenteredGraph(title,center,orbital,R,s=0,f=-1,tilt=30,rot=-100):
     ax.view_init(tilt, rot)
     plt.show()
     
-    
-#def TimeChanges(t):
     
     
 
@@ -560,7 +494,7 @@ ida = Planet("Ida",mIda,aIda,eIda)
 probe = Probe("Probe",mProbe,aProbe,eProbe)
 
 """(x,y,z,vx,vy,vz)"""
-pj = 4.944798694191398
+pj = 4.944798694191398 #perihelion Jupiter
 vpj = 2.8907183743443947
 phiJ = 294*pi/180
 thJ = -1.3*pi/180
@@ -583,36 +517,21 @@ pe = .9880100758
 vpe = 6.388897208268812
 phiE = 0*pi/180
 thE = 0*pi/180
-#earth.vars = array([.9880100758,0,sun.vars[2],0,6.388897208268812,sun.vars[5]])
 earth.setupVars(pe,vpe,phiE,thE)
-print("hwowefn:",earth.vars)
 earth.vars[2] = sun.vars[2]
 print(earth.vars)
-"""print(earth.vars[0]-.9880100758)
-print(earth.vars[1])
-print(earth.vars[2]- sun.vars[2])
-print(earth.vars[3])
-print(earth.vars[4]-6.388897208268812)
-print(earth.vars[5]-sun.vars[5])"""
 
 
 pI = 2.74341
 vpI = 3.8705450693648475
 phiI = 241*pi/180 #110
 thI = -1.132*pi/180
-#ida.vars = array([-0.9381193488120602+sun.vars[0],2.5774617276146103,-0.05419843127580242,
-                  #-3.6364127985602646,-1.3235460181805958+0.0027590699381280052,0.07646587611433431])
 ida.setupVars(pI,vpI,phiI,thI)
 print("init vel ida:",findInitVelocity(aIda,eIda))
 
-#print(earth.vars[0])
-#probe.vars = array([earth.vars[0]+0.0002832326409576363,0,earth.vars[2],0,earth.vars[4]+0.646996,earth.vars[5]])
+
 probe.setupPos(earth.vars[0],earth.vars[1],earth.vars[2],earth.vars[3],earth.vars[4],earth.vars[5])
 probe.setupStages()
-#print(probe.vars[0])
-#probe.setVy(earth.vars[3]+0.646996)
-#probe.vx.append(0)
-#probe.vy.append(0)
 
 
 """solve numerical problem"""
@@ -627,8 +546,7 @@ jupiter.setupV(dt,array([sun]))
 earth.setupV(dt,array([sun,jupiter]))
 ida.setupV(dt,array([sun,jupiter,earth]))
 probe.setupV(dt,array([earth,sun,jupiter]))
-#print(earth.vars[4],probe.vars[4])
-#print(probe.vars[0])
+
 ts1 = 10318#6205
 ts2 = ts1+9
 ts2f = ts2+18#+11
@@ -710,25 +628,13 @@ for i in range(557100):#10001*numOrbits):#52970#170278
     
     #probe.leapFrog(dt,array([earth,sun,jupiter]))
     probe.probeDirector(i,dt)
-"""When making the probe part CREATE a function that will intake the start burn times
-or durations so that there are not as many if else statements"""
-print(probe.vars[0])
-m = 0
-mi = 0
-print(probe.x[0],earth.x[0])
-for i in range(len(probe.x)):
-    if probe.x[i]-earth.x[i] < m:
-        m = probe.x[i]-earth.x[i]
-        mi = i
-print(m,mi)
+
 
 """graph orbits"""
-#graph(bodies, startTime, endTime, Tilt, Rotation)
-graph(" ",array([sun,jupiter,earth,ida,probe]))
-graph(" ",array([sun,jupiter,earth,ida,probe]),tilt=0)
-graph(" ",array([sun,jupiter,earth,ida,probe]),tilt=90)
-
-#graph(array([sun,earth,ida]),0,5000,80,-90)
+#graph(title, bodies, startTime=, endTime=, Tilt=, Rotation=)
+graph("A",array([sun,jupiter,earth,ida,probe]))
+graph("B",array([sun,jupiter,earth,ida,probe]),tilt=0)
+graph("C",array([sun,jupiter,earth,ida,probe]),tilt=90)
 
 CenteredGraph("Earth Centered",earth,probe,0.0000425875,probe.ts1,probe.ts2f+10)
 CenteredGraph("Earth Centered rotated",earth,probe,0.0000425875,probe.ts1,probe.ts2f+10,rot=-170,tilt=5)
@@ -746,17 +652,6 @@ s = probe.tal
 f = probe.tal+probe.taldur
 CenteredGraph("Ida Landing",ida,probe,8.021505*10**-8,s,f,tilt=90)
 graph("probe",array([ida]),s,f,tilt=90)
-
-print("probe v:",sqrt((array(probe.vx[s:f]) - array(ida.vx[s:f]))**2 
-                      + array((probe.vy[s:f]) - array(ida.vy[s:f]))**2 
-                      + array((probe.vz[s:f]) - array(ida.vz[s:f]))**2))
-print("r", (sqrt((array(probe.x[s:f]) - array(ida.x[s:f]))**2 
-                      + array((probe.y[s:f]) - array(ida.y[s:f]))**2 
-                      + array((probe.z[s:f]) - array(ida.z[s:f]))**2) - 8.021505*10**-8)*149597870700)
-
-print("v prop",(array(probe.vx[s:f]) - array(ida.vx[s:f]))
-                      ,array((probe.vy[s:f]) - array(ida.vy[s:f])) 
-                      ,array((probe.vz[s:f]) - array(ida.vz[s:f])))
 
 s = probe.tadep
 f = probe.tadep+probe.tadepdur
@@ -787,10 +682,7 @@ plt.show()
 graph("Earth approach",array([earth,probe]),s,f,tilt=90)
 CenteredGraph("Earth Centered",earth,probe,0.0000425875,s,f,tilt=90)
 CenteredGraph("Earth Centered",earth,probe,0.0000425875,s,f,tilt=0)
-"""
-print("probe v:",sqrt((array(probe.vx[s:f]) - array(earth.vx[s:f]))**2 
-                      + array((probe.vy[s:f]) - array(earth.vy[s:f]))**2 
-                      + array((probe.vz[s:f]) - array(earth.vz[s:f]))**2))"""
+
 """
 graph(array([sun]),tilt=0)
 graph(array([ida]),tilt=0)
